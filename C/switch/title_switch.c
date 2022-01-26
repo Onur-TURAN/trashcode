f4bf29bda481fbc77c5bdf821190f2c04a65c1a8
Onur-TURAN <onurturan.t@gmail.com>
2022-01-26 04:25:17 +0300

    sertifika version 2

---
+++ C/switch/title_switch.c
@@ -0,0 +1,17 @@
+#include<stido.h>
+#include<stdlib.h>
+#include<math.h>
+
+/*
+Template, Mail atılacak liste ve eğitmen sayısına göre eğitmen bilgileri
+*/
+/*
+openGL ile template üzerinden veriler almamız gerekiyor
+*/
+
+/*
+Seçime göre dosya işlemleri
+1. seçim mail listesinin kopyasını kaydet ve bilgileri sertifika sistemine göm
+2. seçim mail listesinin ve gönderilen sertifikaların kopyasını oluştur ve sistemi çalıştır
+3. seçim hiçbir şeyin kopyasını almadan sisteme bilgilerle çalıştır.
+*/
