66052798600a1b0500274f186ea1c841f7132942
Onur-TURAN <onurturan.t@gmail.com>
2022-01-26 04:58:41 +0300

    c version2.1

--- C/switch/title_switch.c
+++ C/switch/title_switch.c
@@ -1,10 +1,35 @@
-#include<stido.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
 /*
 Template, Mail atılacak liste ve eğitmen sayısına göre eğitmen bilgileri
 */
+int main()
+{
+	char A, B, C;
+	printf("Template için dizin yolunu giriniz.\n");
+	scanf("&s", A);
+	printf("Mail listesinin dizin yolunu giriniz.\n");
+	scanf("&s", B;
+	git:printf("Lütfen eğitmrn bilgilrtini giriniz\n");
+	C = getchar();
+	/* eğitmen sayısını seçiyor */
+	switch(getchar())
+	{
+		case 1:printf("1 eğitmenli durumu seçtiniz"); scanf("&s", C); break;
+		case 2:printf("2 eğitmen durumu seçtiniz"); scanf("&s", C); break;
+		default:printf("HATA"); goto git;
+	}
+		if (getchar(1))
+		{
+			printf("seçim");
+		else
+			printf("olmadı");
+		}
+
+}
+
 /*
 openGL ile template üzerinden veriler almamız gerekiyor
 */
